b93365492e4738240d916fcbf9cd71610316c923
anoopreddykunta <anoopreddy240@gmail.com>
2025-01-13 15:31:42 -0800

    Adding fixes to deploy for CORS

--- src/index.ts
+++ src/index.ts
@@ -9,25 +9,15 @@ import { authRouter } from './routes/authRoutes';
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+// Configure CORS
+const corsOptions = {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
